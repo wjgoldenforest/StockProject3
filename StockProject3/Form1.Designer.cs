25c99249fa178b171ec8838abec5b13a3b24776a
wjgoldenforest <cmumism@naver.com>
2019-07-12 17:12:44 +0900

    first commit

---
+++ StockProject3/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace StockProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.itemNameLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.priceListBox = new System.Windows.Forms.ListBox();
+            this.volumeListBox = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.currentPriceLabel = new System.Windows.Forms.Label();
+            this.fluctuationLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(737, 619);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(73, 31);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(787, 628);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.searchTextBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.searchButton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.itemNameLabel, 2, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(787, 35);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(3, 3);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(294, 28);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(303, 3);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(94, 29);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "검색";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // itemNameLabel
+            // 
+            this.itemNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.itemNameLabel.BackColor = System.Drawing.Color.White;
+            this.itemNameLabel.Location = new System.Drawing.Point(403, 3);
+            this.itemNameLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.itemNameLabel.Name = "itemNameLabel";
+            this.itemNameLabel.Size = new System.Drawing.Size(381, 29);
+            this.itemNameLabel.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.priceListBox, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.volumeListBox, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 70);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(787, 558);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // priceListBox
+            // 
+            this.priceListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.priceListBox.FormattingEnabled = true;
+            this.priceListBox.ItemHeight = 18;
+            this.priceListBox.Location = new System.Drawing.Point(1, 1);
+            this.priceListBox.Margin = new System.Windows.Forms.Padding(0);
+            this.priceListBox.Name = "priceListBox";
+            this.priceListBox.Size = new System.Drawing.Size(392, 544);
+            this.priceListBox.TabIndex = 0;
+            // 
+            // volumeListBox
+            // 
+            this.volumeListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.volumeListBox.FormattingEnabled = true;
+            this.volumeListBox.ItemHeight = 18;
+            this.volumeListBox.Location = new System.Drawing.Point(394, 1);
+            this.volumeListBox.Margin = new System.Windows.Forms.Padding(0);
+            this.volumeListBox.Name = "volumeListBox";
+            this.volumeListBox.Size = new System.Drawing.Size(392, 544);
+            this.volumeListBox.TabIndex = 1;
+            this.volumeListBox.SelectedIndexChanged += new System.EventHandler(this.volumeListBox_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.currentPriceLabel);
+            this.flowLayoutPanel1.Controls.Add(this.fluctuationLabel);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 38);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(781, 29);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // currentPriceLabel
+            // 
+            this.currentPriceLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.currentPriceLabel.Location = new System.Drawing.Point(3, 0);
+            this.currentPriceLabel.Name = "currentPriceLabel";
+            this.currentPriceLabel.Size = new System.Drawing.Size(100, 23);
+            this.currentPriceLabel.TabIndex = 0;
+            this.currentPriceLabel.Text = "현재가";
+            this.currentPriceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fluctuationLabel
+            // 
+            this.fluctuationLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.fluctuationLabel.Location = new System.Drawing.Point(109, 0);
+            this.fluctuationLabel.Name = "fluctuationLabel";
+            this.fluctuationLabel.Size = new System.Drawing.Size(100, 23);
+            this.fluctuationLabel.TabIndex = 1;
+            this.fluctuationLabel.Text = "등략율";
+            this.fluctuationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 652);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label itemNameLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.ListBox priceListBox;
+        private System.Windows.Forms.ListBox volumeListBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label currentPriceLabel;
+        private System.Windows.Forms.Label fluctuationLabel;
+    }
+}
+
---
+++ StockProject3/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockProject3
+{
+    public partial class Form1 : Form
+    {
+        string currentItemCode = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            searchButton.Click += ButtonClicked; // 검색 버튼을 눌릴 경우
+
+            axKHOpenAPI1.OnEventConnect += OnEventConnect; // 로그인 결과를 받는 이벤트 함수를 먼저 등록해준다.
+            axKHOpenAPI1.OnReceiveTrData += OnReceiveTrData; // TR데이터 요청 후 결과 콜백함수 등록
+            axKHOpenAPI1.OnReceiveRealData += OnReceiveRealData; // 실시간 정보 요청 결과 콜백함수 등록
+
+            axKHOpenAPI1.CommConnect();  // 1. 로그인창 열기
+
+            priceListBox.DrawItem += ListBoxDrawItem;
+            priceListBox.DrawMode = DrawMode.OwnerDrawVariable;
+            priceListBox.ItemHeight = priceListBox.Height / 20;
+
+            volumeListBox.DrawItem += ListBoxDrawItem;
+            volumeListBox.DrawMode = DrawMode.OwnerDrawVariable;
+            volumeListBox.ItemHeight = volumeListBox.Height / 20;
+        }
+
+        void ListBoxDrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (sender.Equals(priceListBox))
+            {
+                try
+                {
+                    if(e.Index < 10)
+                        e.Graphics.FillRectangle(Brushes.LightSteelBlue, new Rectangle(e.Bounds.X, e.Bounds.Y, e.Bounds.Width, e.Bounds.Height));
+                    else
+                        e.Graphics.FillRectangle(Brushes.LightPink, new Rectangle(e.Bounds.X, e.Bounds.Y, e.Bounds.Width, e.Bounds.Height));
+
+                    String value = priceListBox.Items[e.Index].ToString();
+                    Brush brush;
+                    if(value[0] == '-')
+                        brush = Brushes.Blue;
+                    else
+                        brush = Brushes.Red;
+
+                    int x = e.Bounds.X + e.Font.Height / 2;
+                    int y = e.Bounds.Y + e.Font.Height / 2;
+
+                    e.Graphics.DrawString(value.Replace("-",""), e.Font, brush, x, y, StringFormat.GenericDefault);
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+            }
+            else if (sender.Equals(volumeListBox))
+            {
+                try
+                {
+                    string value = volumeListBox.Items[e.Index].ToString();
+
+                    int x = e.Bounds.X + e.Font.Height / 2 + e.Bounds.Width / 2;
+                    int y = e.Bounds.Y + e.Font.Height / 2;
+
+                    e.Graphics.DrawString(value, e.Font, Brushes.Black, x, y, StringFormat.GenericDefault);
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+            }
+        }
+
+        void OnReceiveRealData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealDataEvent e)
+        {
+            if (e.sRealType == "주식호가잔량")
+            {
+                priceListBox.Items.Clear();
+                volumeListBox.Items.Clear();
+
+                if (e.sRealKey.Equals(currentItemCode))
+                {
+                    for (int i = 50; i >= 41; i--)
+                    {
+                        int 매도호가 = int.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, i));
+                        priceListBox.Items.Add(매도호가);
+                    }
+                    for (int i = 70; i >= 61; i--)
+                    {
+                        int 매도잔량 = int.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, i));
+                        volumeListBox.Items.Add(매도잔량);
+                    }
+                    for (int i = 51; i <= 60; i++)
+                    {
+                        int 매수호가 = int.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, i));
+                        priceListBox.Items.Add(매수호가);
+                    }
+                    for (int i = 71; i <= 80; i++)
+                    {
+                        int 매수잔량 = int.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, i));
+                        volumeListBox.Items.Add(매수잔량);
+                    }
+
+                }
+
+            }
+            if (e.sRealType == "주식체결")
+            {
+                currentPriceLabel.Text = axKHOpenAPI1.GetCommRealData(e.sRealType, 10).ToString();
+                fluctuationLabel.Text = axKHOpenAPI1.GetCommRealData(e.sRealType, 12).ToString() + "%";
+            }
+        }
+
+        // 로그인을 요청 결과 (콜백함수)
+        void OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            // 로그인 요청 결과
+            if (e.nErrCode == 200)
+            {
+
+            }
+            else if (e.nErrCode == 201)
+            {
+            }
+        }
+
+
+        // TR코드 요청 결과 (콜백함수)
+        void OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            // TR코드 요청 결과 (RQ Name으로 구별)
+            if (e.sRQName == "주식호가")
+            {
+                priceListBox.Items.Clear();
+                volumeListBox.Items.Clear();
+
+                for (int i = 10; i > 1; i--)
+                {
+                    int 차선호가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매도"+ i + "차선호가"));
+                    priceListBox.Items.Add(차선호가);
+
+                    if (i == 6)
+                    {
+                        int 차선잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매도" + i + "우선잔량"));
+                        volumeListBox.Items.Add(차선잔량);
+                    }
+                    else
+                    {
+                        int 차선잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매도" + i + "차선잔량"));
+                        volumeListBox.Items.Add(차선잔량);
+                    }
+                }
+
+                int 매도최우선호가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매도최우선호가"));
+                priceListBox.Items.Add(매도최우선호가);
+                int 매도최우선잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매도최우선잔량"));
+                volumeListBox.Items.Add(매도최우선잔량);
+                
+                int 매수최우선호가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수최우선호가"));
+                priceListBox.Items.Add(매수최우선호가);
+                int 매수최우선잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수최우선잔량"));
+
+                for (int i = 2; i < 11; i++)
+                {
+                    if (i == 6)
+                    {
+                        int 호가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수" + i + "우선호가"));
+                        priceListBox.Items.Add(호가);
+                        int 잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수" + i + "우선잔량"));
+                        volumeListBox.Items.Add(잔량);
+                    }
+                    else
+                    {
+                        int 호가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수" + i + "차선호가"));
+                        priceListBox.Items.Add(호가);
+                        int 잔량 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "매수" + i + "차선잔량"));
+                        volumeListBox.Items.Add(잔량);
+                    }
+                }
+                //MessageBox.Show("매도최우선호가: " + 매도최우선호가 + "  ,매수최우선호가: " + 매수최우선호가);
+            }
+        }
+
+        void ButtonClicked(object sender, EventArgs e)
+        {
+            if (sender.Equals(searchButton))
+            {
+                string itemCode = searchTextBox.Text;
+                itemNameLabel.Text = axKHOpenAPI1.GetMasterCodeName(itemCode);
+                currentItemCode = itemCode;
+
+                // 2. TR코드 값관련 결과 요청
+                axKHOpenAPI1.SetInputValue("종목코드", itemCode);
+                int res = axKHOpenAPI1.CommRqData("주식호가", "opt10004", 0, "5001");
+
+                if (res == 0)
+                {
+                    Console.WriteLine("주식호가 요청 성공");
+                }
+                else
+                {
+                    Console.WriteLine("주식호가 요청 실패");
+                }
+
+            }
+        }
+
+        private void volumeListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
